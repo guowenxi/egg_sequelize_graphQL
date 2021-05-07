2201d7b2f22e0d5feb29278baee3f011421bb8e8
code_hunter <200894001@qq.com>
2021-05-07 16:16:28 +0800

    Add files via upload

---
+++ app/graphql/user/db.js
@@ -0,0 +1,4 @@
+module.exports = users = [
+    { id: 1, name: "John Doe", email: "john@gmail.com", age: 22 ,  },
+    { id: 2, name: "Jane Doe22222", email: "jane@gmail.com123123", age: 23 }
+  ];
---
+++ app/graphql/user/resolver.js
@@ -0,0 +1,13 @@
+
+var users = require("./db");
+module.exports = {
+  Query: {
+    user: (parent, { id }, context, info) => {
+      return users.find(user => user.id == id);
+    },
+    users: (parent, args, context, info) => {
+      return users;
+    }
+
+  }
+};
